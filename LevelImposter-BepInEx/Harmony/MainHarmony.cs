115925da11d38283c3323443c800f1385f253579
blaidev <85421187+blaidev@users.noreply.github.com>
2021-11-23 22:06:20 -0500

    fix

--- LevelImposter-BepInEx/Harmony/MainHarmony.cs
+++ LevelImposter-BepInEx/Harmony/MainHarmony.cs
@@ -11,7 +11,7 @@ namespace LevelImposter
     [BepInProcess("Among Us.exe")]
     public class MainHarmony : BasePlugin
     {
-        public const string VERSION = "0.3.3";
+        public const string VERSION = "0.3.4";
         public const string ID = "com.DigiWorm.LevelImposter";
 
         public HarmonyLib.Harmony Harmony { get; } = new HarmonyLib.Harmony(ID);
